788e5371b4a0bbb815448ccbe0116f405f4c1c45
beyzaerrdem <beyzardm502@gmail.com>
2022-03-13 14:38:58 +0300

    added Partial Views

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x=>x.Description).NotEmpty().WithMessage("Açıklamayı boş bırakmayınız.");
+
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("En az 50 karakter girmelisiniz.");
+
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Başlık giriniz.");
+
+
+        }
+    }
+}
