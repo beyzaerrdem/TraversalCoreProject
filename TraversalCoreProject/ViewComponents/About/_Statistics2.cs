5bae72b8a2ca31af43d8ab3a682bb8b62924cacf
beyzaerrdem <beyzardm502@gmail.com>
2022-03-22 21:38:02 +0300

    update

---
+++ TraversalCoreProject/ViewComponents/About/_Statistics2.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.About
+{
+    public class _Statistics2:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            using var c = new Context();
+            ViewBag.s1 = "1730";
+            ViewBag.s2 = "720";
+            ViewBag.s3 = "30";
+            return View();
+        }
+    }
+}
