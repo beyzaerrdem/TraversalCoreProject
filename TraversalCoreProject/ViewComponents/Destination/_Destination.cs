02ea1cdc0a43b848ec22ffcce4cb5f44ca01b671
beyzaerrdem <beyzardm502@gmail.com>
2022-03-23 13:04:27 +0300

    updated destinations page

---
+++ TraversalCoreProject/ViewComponents/Destination/_Destination.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Destination
+{
+    public class _Destination:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.GetList();
+            return View(values);
+        }
+    }
+}
